8e28cbe1971e991945518bd44253cc7d390f9907
TracyChien <tracychien@torchchurch.com>
2021-09-02 15:54:26 +0800

    add surroundings.html & course.html

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -2,7 +2,7 @@ $(function() {
   console.log('Hello Bootstrap5');
 });
 // var swiper = new Swiper(".mySwiper", {});
-
+// index.html
 var indexCourseSwiper = new Swiper(".indexCourseSwiper", {
   slidesPerView: 2,
   spaceBetween: 15,
@@ -30,7 +30,7 @@ var indexCourseSwiper = new Swiper(".indexCourseSwiper", {
     },
   },
 });
-
+// index.html
 var feedbackSwiper = new Swiper(".feedbackSwiper", {
   slidesPerView: 1,
   spaceBetween: 8,
@@ -54,6 +54,34 @@ var feedbackSwiper = new Swiper(".feedbackSwiper", {
         fill: 'row'
       },
       spaceBetween: 30
+    },
+  },
+});
+
+// surroundings.html courseSwiper
+var courseSwiper = new Swiper(".courseSwiper", {
+  slidesPerView: 1.4,
+  spaceBetween: 30,
+  grid: {
+    rows: 1,
+    fill: 'row'
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      grid: {
+        rows: 1,
+        fill: 'row'
+      },
+      spaceBetween: 30,
+    },
+    992: {
+      slidesPerView: 4.5,
+      grid: {
+        rows: 1,
+        fill: 'row'
+      },
+      spaceBetween: 30,
     },
   },
 });
